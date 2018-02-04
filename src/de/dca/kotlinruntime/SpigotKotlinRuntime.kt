5911bed18d63b1623629c6b959dac295b3eb74a3
Alexander Menze <a.menze@polipol.de>
2018-02-04 13:48:24 +0100

    Added load parameter to plugin.yml and added kotlin runtime to output

---
+++ src/de/dca/kotlinruntime/SpigotKotlinRuntime.kt
@@ -0,0 +1,14 @@
+package de.dca.kotlinruntime
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SpigotKotlinRuntime : JavaPlugin() {
+
+    override fun onEnable(){
+
+    }
+
+    override fun onDisable() {
+
+    }
+}
